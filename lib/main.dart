4256996b5c83fb63fa5a1ab19651082a0fe12f9c
hanhan1310 <vuhoangan12102003@gmail.com>
2024-06-16 18:03:49 +0700

    update_main

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Chest_Board(),
+      home: const Demo_Grid_View(),
     );
   }
 }
