34979364da0867a72d62e676c8f73b65ca91c17e
hanhan1310 <vuhoangan12102003@gmail.com>
2024-06-16 18:04:25 +0700

    update_class_image

--- lib/image_class.dart
+++ lib/image_class.dart
@@ -1,3 +1,9 @@
 class Image_insert{
   static const String gojo = "assets/image/gojo.png";
+  static const String frog1 = "assets/image/frog.webp";
+  static const String frog2 = "assets/image/frog2.jpg";
+  static const String cat1 = "assets/image/cat.png";
+  static const String cat2 = "assets/image/cat2.jpg";
+  static const String dog1 = "assets/image/dog1.gif";
+  static const String dog2 = "assets/image/dog2.jpg";
 }
